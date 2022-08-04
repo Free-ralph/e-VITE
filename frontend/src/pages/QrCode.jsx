d0bdf1981edfee3986c3b2bd4d6f39d54422adc2
Free-Ralph <okosachideragia@gmail.com>
2022-08-04 12:03:11 +0100

    made changes

--- frontend/src/pages/QrCode.jsx
+++ frontend/src/pages/QrCode.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
-
+import QrCodeImage from '../qr_code.png'
 
 const QrCode = () => {
   return (
@@ -9,7 +9,7 @@ const QrCode = () => {
         <div className='h-screen w-screen bg-secondary '>
           <div className='flex flex-col justify-center items-center w-full h-full'>
             <div className='w-60 h-60 bg-white'>
-              <img src='/images/qr_code.png' alt='Qr Code' className=''/>
+              <img src={QrCodeImage} alt='Qr Code' className=''/>
             </div>
             <p className='text-center mt-12 md:w-1/4 font-semibold'>Scanning this will bring you to this Site's wedding program page. This Qr Code can also be found on the wedding
             card and banner</p>
